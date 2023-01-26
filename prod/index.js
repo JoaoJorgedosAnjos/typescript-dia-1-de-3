b21821497997e209e32e889b8221ef5daab5937d
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-26 10:18:20 -0300

    aula union type

--- prod/index.js
+++ prod/index.js
@@ -1,7 +1,26 @@
 "use strict";
-//ALIAS TYPE
-const user = {
-    fisrtName: "João",
-    age: 35
-};
-console.log(user);
+// ALIAS TYPE
+// type Users = {
+//     fisrtName:string
+//     age:number
+// }
+// const user:Users = {
+//     fisrtName: "João",
+//     age:35
+// }
+// console.log(user);
+//Union type
+function userInput(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinePrices = userInput(10, 20);
+console.log(combinePrices);
+const combineName = userInput("Apple", "Avocado");
+console.log(combineName);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-//ALIAS TYPE
+// ALIAS TYPE
 
-type Users = {
-    fisrtName:string
-    age:number
+// type Users = {
+//     fisrtName:string
+//     age:number
+// }
+
+// const user:Users = {
+//     fisrtName: "João",
+//     age:35
+// }
+// console.log(user);
+
+//Union type
+
+function userInput(input1: number | string, input2: number | string) {
+    let result
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2
+    } else {
+        result = input1.toString() + input2.toString()
+    }
+    return result
 }
 
-const user:Users = {
-    fisrtName: "João",
-    age:35
-}
-console.log(user);
+const combinePrices = userInput(10, 20)
+console.log(combinePrices);
+
+const combineName = userInput("Apple", "Avocado")
+console.log(combineName);
