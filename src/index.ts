4eb7e338407baf1e992fbc6d429e6acb8d8fa6b2
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-26 10:40:38 -0300

    aula Literal type

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,28 @@
 // }
 // console.log(user);
 
-//Union type
+// //Union type
 
-function userInput(input1: number | string, input2: number | string) {
-    let result
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result
-}
+// function userInput(input1: number | string, input2: number | string) {
+//     let result
+//     if (typeof input1 === "number" && typeof input2 === "number") {
+//         result = input1 + input2
+//     } else {
+//         result = input1.toString() + input2.toString()
+//     }
+//     return result
+// }
 
-const combinePrices = userInput(10, 20)
-console.log(combinePrices);
+// const combinePrices = userInput(10, 20)
+// console.log(combinePrices);
 
-const combineName = userInput("Apple", "Avocado")
-console.log(combineName);
+// const combineName = userInput("Apple", "Avocado")
+// console.log(combineName);
+
+//LITERAL TYPE
+
+let productPrice: 10 | 20 | 30
+let productSiza: "S" | "M" | "L"
+
+productPrice = 20
+productSiza = "M"
