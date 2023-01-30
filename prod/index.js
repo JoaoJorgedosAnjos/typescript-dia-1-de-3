9437cea70990414bd6249bccdee8085b6e1dd6c7
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-30 09:54:19 -0300

    Day 3 - Exercise 3

--- prod/index.js
+++ prod/index.js
@@ -47,6 +47,13 @@
 // let pi = 3.14159;
 // let tau = pi * 2;
 // console.log(`${tau} is ${pi} times two.`);
-// Day 3 - Exercise 2
-let pie = 'blueberry';
-console.log(`I like to eat ${pie}-flavored pie.`);
+// // Day 3 - Exercise 2 
+// // pie é tipo any, transformar em tipo string
+// em typescript colocar obrigatóriamente o tipo em cada variável
+// let pie:string
+// pie = 'blueberry'
+// console.log(`I like to eat ${pie}-flavored pie.`)
+// Day 3 - Exercise 3
+let isDouglas;
+isDouglas = true;
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`);
